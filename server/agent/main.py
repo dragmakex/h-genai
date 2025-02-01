c2398c829d2d3c01a9ea0668ad818bcad40bf47f
Luis <luis.wiedmann@gmail.com>
2025-02-01 01:49:40 +0100

    tried other models, mistral is the best

--- server/agent/main.py
+++ server/agent/main.py
@@ -7,7 +7,7 @@ load_dotenv()
 
 aws_access_key_id=os.getenv("AWS_ACCESS_KEY_ID")
 aws_secret_access_key=os.getenv("AWS_SECRET_ACCESS_KEY")
-aws_region_name=os.getenv("AWS_DEFAULT_REGION")
+aws_region_name=os.getenv("AWS_DEFAULT_REGION") 
 
 generator = AmazonBedrockChatGenerator(model="mistral.mistral-large-2407-v1:0")
 messages = [ChatMessage.from_system("You are a helpful assistant that answers question."), ChatMessage.from_user("What's Natural Language Processing? Be brief.")]
