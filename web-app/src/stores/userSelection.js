6e1d6e0c5fef9096f0a22e568003897ddb7f43f4
Jan <ja.dorn@web.de>
2025-02-01 22:18:23 +0100

    added new variable to stores

--- web-app/src/stores/userSelection.js
+++ web-app/src/stores/userSelection.js
@@ -6,7 +6,8 @@ export const useUserSelectionStore = defineStore('userSelection', {
     selected_municipality_code: '',
     selected_inter_municipality: '',
     selected_inter_municipality_code: '',
-    selected_siren: ''
+    selected_siren: '',
+    selected_reference_sirens: [],
   }),
   actions: {
     setSelectedMunicipality(municipality) {
@@ -15,6 +16,7 @@ export const useUserSelectionStore = defineStore('userSelection', {
       this.selected_inter_municipality = municipality.inter_name || ''
       this.selected_inter_municipality_code = municipality.inter_code || ''
       this.selected_siren = municipality.siren || ''
+      this.selected_reference_sirens = municipality.reference_sirens?.map(ref => ref.siren) || []
     }
   }
 })
